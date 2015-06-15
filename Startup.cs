0e0ca3dfea7ea0c27244038d2ed85cc642df0a7b
RustyFrost <jproelofse@gmail.com>
2015-06-15 08:58:00 +0200

    Dota Init

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Dota2Nube.Startup))]
+namespace Dota2Nube
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
