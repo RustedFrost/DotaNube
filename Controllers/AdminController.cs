752b376d084fc791cc790e674970fb13e7f3f209
Lourens Du Toit <dutoitl@gmail.com>
2015-06-19 09:15:34 +0200

    Updated

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Dota2Nube.Controllers
+{
+    public class AdminController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Countries()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Dota2Nube.Data/Providers/Countries/CountryProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dota2Nube.Data;
+
+namespace Dota2Nube.Data.Providers.Countries
+{
+    class CountryProvider
+    {
+        public List<Country> getAll()
+        {
+            Data.Dota2NubeConn1 db = new Dota2NubeConn1();
+            List<Country> lstCountry = null;
+            using (db)
+            {
+                lstCountry = db.Country.ToList();
+            }
+            return lstCountry;
+        }
+        public Country Save(Country objCountry)
+        {
+            Data.Dota2NubeConn1 db = new Dota2NubeConn1();
+            Country updCountry = null;
+            using (db)
+            {
+                if (objCountry.CountryId == 0)
+                {
+                    objCountry = db.Country.Add(objCountry);
+                }
+                else
+                {
+                    updCountry = db.Country.Where(x => x.CountryId == objCountry.CountryId).FirstOrDefault();
+                    updCountry.CountryName = objCountry.CountryName;
+                    updCountry.Users = objCountry.Users;
+                }
+                db.SaveChanges();
+            }
+            return objCountry;
+        }
+    }
+}
