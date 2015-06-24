aaefb62fffbff97dee6969ab6ea70e191e07e26b
RustyFrost <jproelofse@gmail.com>
2015-06-24 08:05:05 +0200

    Demo For lourens

---
+++ Controllers/PoolController.cs
@@ -0,0 +1,40 @@
+﻿using Dota2Nube.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Dota2Nube.Controllers
+{
+    public class PoolController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetPools()
+        {
+            List<PoolModel> pools = new List<PoolModel>();
+
+            Data.Dota2NubeConn1 db = new Data.Dota2NubeConn1();
+            
+            var dbpools = db.Pool.ToList();
+
+            foreach (var item in dbpools)
+            {
+                pools.Add(new PoolModel
+                {
+                    PoolId = item.PoolId,
+                    PoolName = item.PoolName
+                });
+            }
+
+            return JsonConvert.SerializeObject(pools);
+        }
+    }
+}
---
+++ Models/PoolModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dota2Nube.Models
+{
+    public class PoolModel
+    {
+        public int PoolId { get; set; }
+        public string PoolName { get; set; }
+        public int UserId { get; set; }
+        public int TournamentId { get; set; }
+
+    }
+}
